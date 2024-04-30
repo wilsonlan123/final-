c3692aee4d048794d74fe5a1414153d63e16294d
Sangho12 <159551341+Sangho12@users.noreply.github.com>
2024-04-30 22:41:30 +0800

    Create Guess_Your_Luck.h

---
+++ Guess_Your_Luck.h
@@ -0,0 +1,12 @@
+#ifndef GUESS_YOUR_LUCK_H
+#define GUESS_YOUR_LUCK_H
+
+#include <string>
+#include <vector>
+#include<list>
+using namespace std;
+
+bool update_range(int ,int ,int numrange[]);
+void Guess_Your_Luck(string, string);
+
+#endif
